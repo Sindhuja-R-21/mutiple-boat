a468c28d02d3355fd626f11d952d99c91b8609b4
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2021-10-12 19:39:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,113 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+
+//bodies-used to create PE bodies
+const Bodies = Matter.Bodies;
+//to create connection
+const Constraint = Matter.Constraint;
+
+//Body-to give props
+const Body=Matter.Body;
+
+var engine, world, backgroundImg;
+var canvas, angle, tower, ground, cannon;
+var balls = [];
+var boats=[];
+
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.gif");
+  towerImage = loadImage("./assets/tower.png");
+  
+}
+
+function setup() {
+  canvas = createCanvas(1200,600);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  angle = -PI / 4;
+  ground = new Ground(0, height - 1, width * 2, 1);
+  tower = new Tower(150, 350, 160, 310);
+  cannon = new Cannon(180, 110, 100, 50, angle);
+
+ 
+
+  
+
+  
+}
+
+function draw() {
+  background(189);
+  image(backgroundImg, 0, 0, width, height);
+
+ 
+
+  Engine.update(engine);
+  ground.display();
+  
+   showBoats();
+
+  for (var i = 0; i < boats.length; i++) {
+     
+    Matter.Body.setVelocity(boats[i].body,{x:-0.9,y:0})
+
+    boats[i].display();
+    
+  }
+
+
+  for (var i = 0; i < balls.length; i++) {
+    showCannonBalls(balls[i], i);
+  }
+
+  cannon.display();
+  tower.display();
+
+  
+}
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    balls.push(cannonBall);
+  }
+}
+
+//function to show the ball
+function showCannonBalls(ball, index) {
+  ball.display();
+  if (ball.body.position.x >= width || ball.body.position.y >= height - 50) {
+    Matter.World.remove(world, ball.body);
+    balls.splice(index, 1);
+  }
+}
+
+
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW) { 
+    balls[balls.length - 1].shoot();
+  }
+}
+function showBoats(){
+ //atleast 1 boat
+  if(boats.length>0){
+    //max 4 boat
+    if(boats.length<4 && boats[boats.length-1].body.position.x<width-300){
+      var positions=[-130,-100,-120,-80]
+      var position=random(positions);
+      boat=new Boat(width,height-100,200,200,position);
+      boats.push(boat);
+   }
+  }
+
+  //no boat is present
+  else{
+    boat=new Boat(width,height-100,200,200,-100);
+    boats.push(boat);
+  }
+}
+
